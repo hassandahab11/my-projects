ec53f25762605bc8a7358570d49e1234bcb5070d
unknown <hassandahab11@gmail.com>
2021-01-28 17:05:35 +0100

    primary version

---
+++ codeJs/script.js
@@ -0,0 +1,20 @@
+// function y(){
+//     var i;
+//     for(i = 1; i<=10; i++){
+//         console.log(8 + " * " + i +" = "+ 8 * i + "  " );
+//     }
+// } y();
+
+
+
+
+		  
+        
+
+function y(){
+    var i;
+    for(i = 1; i<=5; i++){
+        console.log(i );
+    }
+} y();
+
